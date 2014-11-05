c303e2ea586e07e6f5320b6f0ec4e0a63cda044a
Jelle Henkens <jelle.henkens@gmail.com>
2014-11-05 09:52:28 -0800

    Adding error handling for the M2X integration

--- pimotion/main.py
+++ pimotion/main.py
@@ -1,6 +1,8 @@
 from pimotion import PiMotion
 from cloudapp import CloudAppAPI
 from m2x.client import M2XClient
+from m2x.errors import APIError
+from requests.exceptions import HTTPError
 import ConfigParser
 
 
@@ -13,10 +15,16 @@ def callback(path):
     url = api.upload(path)
     print 'Public URL: ' + url
 
-    client = M2XClient(Config.get('m2x', 'api_key'))
-    feed = client.feeds.details(Config.get('m2x', 'feed_id'))
-    result = feed.add_values({Config.get('m2x', 'stream'): [{'value': url}]})
-    print result
+    try:
+        client = M2XClient(Config.get('m2x', 'api_key'))
+        feed = client.feeds.details(Config.get('m2x', 'feed_id'))
+        result = feed.add_values({Config.get('m2x', 'stream'): [{'value': url}]})
+        print "Posted URL to M2X channel %s" % Config.get('m2x', 'stream')
+    except HTTPError, e:
+        print 'ERROR' + e.message
+    except APIError, e:
+        errors = ', '.join("%s: %r" % (key, val) for (key, val) in e.errors.iteritems())
+        print "ERROR: %s - %s" % (e.message, errors)
 
 motion = PiMotion(verbose=True, post_capture_callback=callback)
 motion.start()
